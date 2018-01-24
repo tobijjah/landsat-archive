0054c8b84f79462b385be5cfb1c739a5344c3035
tobijjah <tobias.seydewitz@posteo.net>
2018-01-24 15:26:36 +0100

    Test sniffer

--- tests/test_landsatArchive.py
+++ tests/test_landsatArchive.py
@@ -6,10 +6,16 @@ from src.landsat import LandsatArchive
 
 
 class TestLandsatArchive(unittest.TestCase):
-    # TODO mock required
+    @unittest.skip('mock required')
     def test_factory(self):
         _tar = LandsatArchive.factory(Path('foo.tar.gz'))
         _zip = LandsatArchive.factory(Path('foo.zip'))
 
         self.assertTrue(isinstance(_tar, TarFile))
         self.assertTrue(isinstance(_zip, ZipFile))
+
+    def test_metadata_sniffer(self):
+        vals = ['foo', 'bar', 'foo/bar/landsat_mtl.txt']
+
+        self.assertEqual('landsat_mtl.txt', LandsatArchive.metadata_sniffer(vals))
+
