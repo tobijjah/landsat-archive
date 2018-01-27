648e0b5e95d1f17609a51d4a399722a8c94f6323
tobijjah <tobias.seydewitz@posteo.net>
2018-01-27 22:12:57 +0100

    Refactoring

--- tests/test_landsatArchive.py
+++ tests/test_landsatArchive.py
@@ -1,21 +1,13 @@
 import unittest
-from pathlib import Path
-from zipfile import ZipFile
-from tarfile import TarFile
 from src.landsat import LandsatArchive
 
 
 class TestLandsatArchive(unittest.TestCase):
-    @unittest.skip('mock required')
-    def test_factory(self):
-        _tar = LandsatArchive.factory(Path('foo.tar.gz'))
-        _zip = LandsatArchive.factory(Path('foo.zip'))
-
-        self.assertTrue(isinstance(_tar, TarFile))
-        self.assertTrue(isinstance(_zip, ZipFile))
+    def test_directory_open(self):
+        self.fail()
 
     def test_metadata_sniffer(self):
         vals = ['foo', 'bar', 'foo/bar/landsat_mtl.txt']
 
-        self.assertEqual('landsat_mtl.txt', LandsatArchive.metadata_sniffer(vals))
+        self.assertEqual('landsat_mtl.txt', LandsatArchive.metadata_sniffer(vals, r'.+_mtl.txt'))
 
