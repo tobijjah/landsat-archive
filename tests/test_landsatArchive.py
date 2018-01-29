ae8dfeadb734edb485fa846a916ca73f642d4a04
tobijjah <tobias.seydewitz@posteo.net>
2018-01-29 19:30:41 +0100

    Simplified testing

--- tests/test_landsatArchive.py
+++ tests/test_landsatArchive.py
@@ -1,13 +1,50 @@
 import unittest
-from landsat.utils import LandsatArchive
+from tests.stubs import L1, L4, L7, L8, OpenStub, FileMock
+from landsat.utils import LandsatArchive, LandsatMetadata, BAND_MAP
 
 
 class TestLandsatArchive(unittest.TestCase):
+    def setUp(self):
+        self.meta_file = L8
+
+        # prepare mock objects
+        file_mock = FileMock(self.meta_file['data'])
+        open_stub = OpenStub(file_mock)
+        opener = open_stub.open
+
+        # inject open stub
+        LandsatMetadata._OPENER = opener
+
+        self.meta = LandsatMetadata('foo')
+        self.meta.parse()
+
     def test_directory_open(self):
-        self.fail()
+        pass
+
+    def test_metadata_read(self):
+        pass
+
+    def test_archive_read(self):
+        pass
 
     def test_metadata_sniffer(self):
         vals = ['foo', 'bar', 'foo/bar/landsat_mtl.txt']
 
         self.assertEqual('landsat_mtl.txt', LandsatArchive.metadata_sniffer(vals, r'.+_mtl.txt'))
 
+    def test_metadata_sniffer_raises(self):
+        pass
+
+    def test_load(self):
+        obj = LandsatArchive('test', self.meta, '', '')
+        obj._load()
+        print(obj._bands)
+
+    def test_dispatch_mapping(self):
+        expected = BAND_MAP[self.meta_file['id']]
+        result = LandsatArchive.dispatch_mapping(self.meta, BAND_MAP)
+
+        self.assertEqual(expected, result)
+
+    def test_dispatch_mapping_raises(self):
+        pass
--- tests/test_landsatMetadata.py
+++ tests/test_landsatMetadata.py
@@ -6,9 +6,12 @@ from landsat.utils import MetadataFileParsingError
 
 class TestLandsatMetadata(TestCase):
     # TODO test _asdict, parse, scanner, init, read
+    # TODO del hard coded test attributes
     def setUp(self):
+        self.meta_file = L7
+
         # prepare mock objects
-        file_mock = FileMock(L8)
+        file_mock = FileMock(self.meta_file['data'])
         open_stub = OpenStub(file_mock)
         opener = open_stub.open
 
@@ -27,15 +30,15 @@ class TestLandsatMetadata(TestCase):
 
     def test_get_with_valid_attributes(self):
         self.assertTrue(isinstance(self.meta.get('PRODUCT_METADATA'), tuple))
-        self.assertTrue(self.meta.get('pROduct_metaDATA', 'spacecraft_id') == 'LANDSAT_8')
+        self.assertTrue(self.meta.get('pROduct_metaDATA', 'spacecraft_id') == self.meta_file['spacecraft'])
 
     def test_get_with_invalid_attributes(self):
-        self.assertTrue(self.meta.get('FOO', 'BAR') is None)
+        self.assertTrue(self.meta.get('foo', 'bar') is None)
 
     def test_iter_group_with_valid_attribute(self):
-        result = list(self.meta.iter_group('PRODUCT_METADATA'))
+        result = list(self.meta.iter_group('product_metadata'))
 
-        self.assertTrue(len(result) == 54)
+        self.assertTrue(len(result) == self.meta_file['product_len'])
 
     def test_iter_group_with_invalid_attribute(self):
         result = list(self.meta.iter_group('foo'))
@@ -43,14 +46,11 @@ class TestLandsatMetadata(TestCase):
         self.assertTrue(result == [])
 
     def test_lexer_with_valid_metadata(self):
-        # stub of valid metadata content
-        mock_gen = ['GROUP = T1', 'ATTR1 = 1', 'END_GROUP = T1', 'GROUP = T2', 'ATTR1 = 1', 'END_GROUP = T2']
-        result = list(LandsatMetadata.lexer(mock_gen))
+        result = list(LandsatMetadata.lexer(self.meta_file['data']))
 
-        self.assertTrue(len(result) == 2)
+        self.assertTrue(len(result) == self.meta_file['groups'])
 
     def test_lexer_with_malicious_metadata(self):
-        # stub of valid metadata content
         mock_gen = ['GROUP = T1', 'ATTR1 = 1', 'END_GROUP = T1', 'GROUP = T2', 'ATTR1 = 1',
                     'GROUP = T3', 'ATTR1 = 1', 'END_GROUP = T3']
         result = list(LandsatMetadata.lexer(mock_gen))
@@ -58,17 +58,15 @@ class TestLandsatMetadata(TestCase):
         self.assertTrue(len(result) == 3)
 
     def test_parser_returns_expected_on_valid_metadata(self):
-        # stub of a valid metadata group
         mock_gen = [['GROUP = TEST1', 'ATTR1 = 1', 'ATTR2 = 2.0', 'ATTR3 = "A TEST"']]
         fields = ('GROUP', 'ATTR1', 'ATTR2', 'ATTR3')
 
         result = LandsatMetadata.parser(mock_gen)
         for item in result:
             self.assertTrue(item, tuple)
-            self.assertEqual(item._fields, fields)
+            self.assertEqual(fields, item._fields)
 
     def test_parser_fails_on_invalid_metadata(self):
-        # stub of a invalid metadata group
         mock_gen = [['GROUP = TEST1'], ['FOO', 'BAR'], []]
 
         with self.assertRaises(MetadataFileParsingError):
